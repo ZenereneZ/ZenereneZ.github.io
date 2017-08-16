b05e08647e178b6ecfb748cc2395e7ada17b88f3
Yuni Fuchioka <Yuni@Yunies-MacBook-Pro-2.local>
2017-08-15 23:37:04 -0700

    Added picture gallery

--- js/agency.js
+++ js/agency.js
@@ -35,4 +35,20 @@
     }
   });
 
+  // Magnific popup calls. Taken from start bootstrap creative theme
+  $('.popup-gallery').magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      tLoading: 'Loading image #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+          enabled: true,
+          navigateByImgClick: true,
+          preload: [0, 1]
+      },
+      image: {
+          tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+      }
+  });
+
 })(jQuery); // End of use strict
